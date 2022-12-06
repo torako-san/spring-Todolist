ae8185a32be47fcb1dd55a4cfa7147995093b009
torako-san <100730549+torako-san@users.noreply.github.com>
2022-12-06 13:45:33 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/entity/Todo.java
@@ -0,0 +1,37 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="todo")
+@Data
+public class Todo {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer id;
+	
+	@Column(name="title")
+	private String title;
+	
+	@Column(name="importname")
+	private Integer importance;
+	
+	@Column(name="urgency")
+	private Integer urgency;
+	
+	@Column(name="deadline")
+	private Date deadline;
+	
+	@Column(name="done")
+	private String done;
+}
