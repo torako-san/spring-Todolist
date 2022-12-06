6664ac77b06e59b0f1c2a631ce1842fb2c4f3cfc
torako-san <100730549+torako-san@users.noreply.github.com>
2022-12-06 15:55:49 +0900

    todoテーブルの検索結果を処理する記述

--- src/main/java/com/example/demo/controller/TodoListController.java
+++ src/main/java/com/example/demo/controller/TodoListController.java
@@ -1,5 +1,26 @@
 package com.example.demo.controller;
 
-public class TodoListController {
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.entity.Todo;
+import com.example.demo.repository.TodoRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Controller
+public class TodoListController {
+	private final TodoRepository todoRepository;
+	
+	@GetMapping("/todo")
+	public ModelAndView showTodoList(ModelAndView mv) {
+		mv.setViewName("todoList");
+		List<Todo>todoList = todoRepository.findAll();
+		mv.addObject("todoList", todoList);
+		return mv;
+	}
 }
