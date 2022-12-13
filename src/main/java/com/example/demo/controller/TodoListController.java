a9364d9eab91c57ee486b28767e8598ece026546
torako-san <100730549+torako-san@users.noreply.github.com>
2022-12-13 15:20:38 +0900

    コントローラーに登録する処理を記述

--- src/main/java/com/example/demo/controller/TodoListController.java
+++ src/main/java/com/example/demo/controller/TodoListController.java
@@ -3,11 +3,17 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.entity.Todo;
+import com.example.demo.form.TodoData;
 import com.example.demo.repository.TodoRepository;
+import com.example.demo.servise.TodoService;
 
 import lombok.AllArgsConstructor;
 
@@ -15,6 +21,8 @@ import lombok.AllArgsConstructor;
 @Controller
 public class TodoListController {
 	private final TodoRepository todoRepository;
+	private final TodoService todoService;
+	
 	
 	@GetMapping("/todo")
 	public ModelAndView showTodoList(ModelAndView mv) {
@@ -22,5 +30,32 @@ public class TodoListController {
 		List<Todo>todoList = todoRepository.findAll();
 		mv.addObject("todoList", todoList);
 		return mv;
+	}
+	
+	@GetMapping("/todo/create")
+	public ModelAndView createTodo(ModelAndView mv) {
+		mv.setViewName("todoForm");
+		mv.addObject("todoData", new TodoData());
+		return mv;
+	}
+	
+	@PostMapping("/todo/create")
+	public ModelAndView createTodo(@ModelAttribute @Validated TodoData todoData,
+			BindingResult result, ModelAndView mv) {
+		boolean isValid = todoService.isValid(todoData, result);
+		if(!result.hasErrors()&& isValid) {
+			Todo todo = todoData.toEntity();
+			todoRepository.saveAndFlush(todo);
+			return showTodoList(mv);
+		} else {
+			mv.setViewName("todoForm");
+			//mav.addObject("todoData", todoData);
+			return mv;
+		}
+	}
+	
+	@PostMapping("/todo/cancel")
+	public String cancel() {
+		return "redirect:/todo";
 	}
 }
